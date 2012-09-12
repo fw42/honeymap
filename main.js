d287fdbf73e26e9a4fb431e6cfbc2e9290fd0ad2
Florian Weingarten <flo@hackvalue.de>
2012-09-12 16:20:26 +0200

    initial

---
+++ main.js
@@ -0,0 +1,26 @@
+function set_map_size() {
+	$('#world-map').width(0.95 * $(document).width());
+	$('#world-map').height(0.8 * $(document).height());
+}
+
+function set_log_size() {
+	$('#log').width(0.5 * $(document).width());
+	$('#log').css("margin-top", 0.025 * $(document).height());
+	$('#log').height(0.15 * $(document).height());
+}
+
+$(window).resize(function(){
+	set_map_size();
+	set_log_size();
+});
+
+set_map_size();
+set_log_size();
+
+$('#world-map').vectorMap({
+	backgroundColor: '#021320',
+	onRegionClick: function(e, c) {
+		$('#log').append($('#world-map').vectorMap('get', 'mapObject').getRegionName(c) + "<br/>");
+		$("#log").scrollTop($("#log")[0].scrollHeight);
+        }
+});
