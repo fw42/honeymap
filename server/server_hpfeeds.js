d7af0d157a3139ddbe9f9febfd9838d17ea141b6
Florian Weingarten <flo@hackvalue.de>
2012-09-18 15:08:08 +0200

    catch exceptions in server

--- server/server_hpfeeds.js
+++ server/server_hpfeeds.js
@@ -1,3 +1,5 @@
+var sys = require('sys');
+var url = require('url');
 var app = require('http').createServer(handler);
 var fs = require('fs');
 var util = require('util');
@@ -23,19 +25,26 @@ feedconn.onready(function() { feedconn.subscribe('geoloc.events'); });
 
 // Serve static content
 function handler (req, res) {
-  req.addListener('end', function() {
-    file.serve(req, res, function(err, result) {
-      if (err) {
-        console.error('Error serving %s - %s', req.url, err.message);
-        if (err.status === 404 || err.status === 500) {
-          file.serveFile(util.format('/%d.html', err.status), err.status, {}, req, res);
-        } else {
-          res.writeHead(err.status, err.headers);
-          res.end();
+  try {
+    console.log('New request: ' + req.connection.remoteAddress + ': ' + url.parse(req.url).href);
+    req.addListener('end', function() {
+      file.serve(req, res, function(err, result) {
+        if (err) {
+          console.error('Error serving %s: %s', req.url, err.message);
+          if (err.status === 404 || err.status === 500) {
+            file.serveFile(util.format('/%d.html', err.status), err.status, {}, req, res);
+          } else {
+            res.writeHead(err.status, err.headers);
+            res.end();
+          }
         }
-      }
+      });
     });
-  });
+  } catch(err) {
+    sys.puts(err);
+    res.writeHead(500);
+    res.end('Internal Server Error');
+  }
 }
 
 // Push feed data to all connected sockets
