42db06243c97f2b3ce9562af025ceb18f0d1dbe9
Florian Weingarten <flo@hackvalue.de>
2012-09-20 17:40:36 +0200

    welcome.js

---
+++ static/welcome.js
@@ -0,0 +1,3 @@
+add_log("<b>Welcome to HoneyMap. This is an ALPHA version! Bug reports welcome :-)</b><br/>");
+add_log("Note that this are not <b>all</b> honeypots of the Honeynet Project,<br/>");
+add_log("only those who voluntarily publish their captures to hpfeeds!<br/><br/>");
