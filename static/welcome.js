c787006b7eb1c1b9a1476854d366ec275b913078
Florian Weingarten <flo@hackvalue.de>
2013-03-15 02:03:18 +0100

    small typo

--- static/welcome.js
+++ static/welcome.js
@@ -1,3 +1,3 @@
 add_log("<b>Welcome to HoneyMap. This is an ALPHA version! Bug reports welcome :-)</b>");
-add_log("Note that this are not <b>all</b> honeypots of the Honeynet Project,");
+add_log("Note that this is not <b>all</b> honeypots of the Honeynet Project,");
 add_log("only those who voluntarily publish their captures to hpfeeds!<br/>");
